a5dc52c26269a9b7becbaa5cb2e2c63aa167d780
Madhavan Mani <maddyprsnl@gmail.com>
2023-03-29 19:11:38 +0530

    Update arangra.py

--- arangra.py
+++ arangra.py
@@ -10,10 +10,10 @@ try:
     build_dir = os.getcwd()
     docker_image = 'owasp/zap2docker-stable '
     command_line = 'touch /zap/wrk/ramanuja.txt'
-    user = 'jenkins'
+    users = 'jenkins'
     vol = '/zap/wrk'
     container_output = docker.containers.run(docker_image, command_line, volumes={build_dir: {
-                    'bind': , 'mode': 'rw'}},user=user)
+                    'bind':vol , 'mode': 'rw'}},user=users)
     print('[SUCCESS]: Scan Completed aranga')
     print(container_output)
 except errors.ContainerError as exc:
